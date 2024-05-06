5b8e9a951d248c4375ed6db033406cf6b8337bad
Jemendieta <mendieta.86@gmail.com>
2024-05-06 18:11:09 -0500

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Jemendieta.js
@@ -0,0 +1,22 @@
+// Este es un comentatio de una sola línea
+/*
+    Este es un comentario
+    de varias
+    líneas
+*/
+
+// Variables
+var nombre = "Jorge";
+let apellido = "Mendieta";
+const sexo = "Masculino";
+
+// Primitivos
+
+let cadena = "hola";
+let numero = 37;
+let booleano = Boolean;
+let indefinido = undefined;
+
+// Imprimir por consola
+
+console.log("Hola, mi lenguaje es JavaScript");
